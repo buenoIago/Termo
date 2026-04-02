893a6291ac7777a43773d5a1ae1307f6168a4135
buenoIago <iagocedup@gmail.com>
2026-04-02 14:21:10 -0300

    first commit

---
+++ Termo.ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main (String[] args)
+    {
+        Console.Clear();
+
+        System.Console.WriteLine("-------------------------------------");
+        System.Console.Write("TERMO");
+        System.Console.WriteLine("- 5 LETRAS");
+        System.Console.WriteLine("-------------------------------------");
+
+
+    }
+}
