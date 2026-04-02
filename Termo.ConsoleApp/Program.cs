ef843aea9cacb06571562e7c010bcdf68d686368
buenoIago <iagocedup@gmail.com>
2026-04-02 14:25:31 -0300

    adicionado geração de palavras aleatória

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Security.Cryptography;
+
+class Program
 {
     static void Main (String[] args)
     {
@@ -9,6 +11,22 @@
         System.Console.WriteLine("- 5 LETRAS");
         System.Console.WriteLine("-------------------------------------");
 
+        string[] palavras = {
+            "abrir", "achar", "água", "ajudar",
+            "amar", "amigo", "beber", "casa",
+            "chorar", "claro", "comer", "correr",
+            "criar", "deixar", "descer", "dever",
+            "entrar", "falar", "fechar", "fogo",
+            "forte", "jogar", "leve", "livro",
+            "metal", "mudar", "nadar", "noite",
+            "odiar", "olhar", "ouvir", "papel",
+            "pedra", "pegar", "pensar", "praia",
+            "pular", "rir", "sair", "sentir",
+            "somar", "subir", "tentar", "tinta",
+            "vento", "verde", "vidro", "viver"
+        };
 
+        int numeroAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
+          
     }
 }
