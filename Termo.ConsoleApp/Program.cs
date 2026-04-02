211e615572d14ec9a2e0175424489c1b47c953ba
buenoIago <iagocedup@gmail.com>
2026-04-02 15:51:28 -0300

    adicionado confirmação de letra correta

--- Termo.ConsoleApp/Program.cs
+++ Termo.ConsoleApp/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Security.Cryptography;
+﻿using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 
 class Program
 {
@@ -8,34 +11,75 @@ class Program
 
         System.Console.WriteLine("-------------------------------------");
         System.Console.Write("TERMO");
-        System.Console.WriteLine("- 5 LETRAS");
+        System.Console.WriteLine(" - 5 LETRAS");
         System.Console.WriteLine("-------------------------------------");
 
         string[] palavras = {
-            "abrir", "achar", "água", "ajudar",
-            "amar", "amigo", "beber", "casa",
-            "chorar", "claro", "comer", "correr",
-            "criar", "deixar", "descer", "dever",
-            "entrar", "falar", "fechar", "fogo",
-            "forte", "jogar", "leve", "livro",
-            "metal", "mudar", "nadar", "noite",
-            "odiar", "olhar", "ouvir", "papel",
-            "pedra", "pegar", "pensar", "praia",
-            "pular", "rir", "sair", "sentir",
-            "somar", "subir", "tentar", "tinta",
-            "vento", "verde", "vidro", "viver"
+            "beber", "abrir", "andar",
+            /*
+            "dizer", "fazer", "olhar",
+            "ouvir", "pedir", "parar", "pegar",
+            "jogar", "viver", "lutar", "nadar",
+            "tocar", "temer", "saber",
+            "poder", "caber", "haver", "medir",
+            "subir", "valer", "bater",
+            "mexer", "lavar", "secar", "colar",
+            "remar", "rezar", "rolar", "morar",
+            "dotar", "notar", "lotar", "botar",
+            "tirar", "gerar", "negar", "jurar",
+            "calar", "selar", "gelar", "velar",
+            "arder"
+            */
         };
 
         int numeroAleatorio = RandomNumberGenerator.GetInt32(palavras.Length);
 
-        string palavraAleatorio = palavras[numeroAleatorio];
+        string palavraAleatoria = palavras[numeroAleatorio];
 
-        System.Console.WriteLine($"Palavra sorteada foi {palavraAleatorio}");
+        System.Console.WriteLine($"Palavra sorteada foi {palavraAleatoria}");
 
         while (true)
         {
-            
-        }
-          
+            Console.ResetColor();
+                
+            System.Console.WriteLine("Digite uma palavra: ");
+            string? palavraDigitada = Console.ReadLine();
+
+            char[] letrasAcertadas = new char[palavraAleatoria.Length];
+
+            for (int caractere = 0; caractere < palavraAleatoria.Length; caractere++)
+            {
+                int letrasCorretas;
+
+                if (caractere < palavraDigitada.Length)
+                {
+                    if (palavraDigitada[caractere] == palavraAleatoria[caractere])
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+
+                    else if (palavraDigitada != palavraAleatoria)
+                    {
+                        
+                        for (letrasCorretas = 0; letrasCorretas < palavraAleatoria.Length; letrasCorretas++)
+                        {
+                            if (palavraDigitada[caractere] == palavraAleatoria[letrasCorretas])
+                            {
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                                break;
+                            }
+                        }   
+                    }
+
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                    }
+
+                    Console.Write(palavraDigitada[caractere] + " ");
+                }                                       
+            }   
+       Thread.Sleep(1000);
+        }          
     }
 }
